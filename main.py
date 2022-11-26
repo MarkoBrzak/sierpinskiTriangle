457323d0e6a3bd6057de0f7165cc46c63f569fcd
Marko Brzak <marko.brzak.5@gmail.com>
2022-11-26 10:46:21 +0100

    Initial commit.

---
+++ main.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+from tkinter import *
+
+
+
+if __name__ == "__main__":
+
+    root = tk.Tk()
+    root.geometry("500x500")
+    root.title("Sierpinski Triangle")
+
+    #scale = Scale(root, from_=0, to=42)
+
+    root.mainloop()
